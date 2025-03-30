6a0cbc2d3cd2a9fa2c05b3c69d3d250304e49d99
danielangelo1 <danielangelo1234@gmail.com>
2025-03-30 14:07:16 -0300

    feat: add baseUrl to tsconfig.json

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import database from "../../../../infra/database";
+import database from "infra/database";
 
 const status = async (request: NextApiRequest, response: NextApiResponse) => {
   const result = await database.query("SELECT 1 +1 as sum");
