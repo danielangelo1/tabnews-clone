3af8b912de2e08053d62d0ef18c5eaba23a29516
danielangelo1 <danielangelo1234@gmail.com>
2025-03-30 13:37:21 -0300

    feat: add database.ts and configs

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,6 +1,9 @@
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import database from "../../../../infra/database";
 
-const status = (request: NextApiRequest, response: NextApiResponse) => {
+const status = async (request: NextApiRequest, response: NextApiResponse) => {
+  const result = await database.query("SELECT 1 +1 as sum");
+  console.log(result.rows);
   response.status(200).json({ status: "ok" });
 };
 
