c62429bb2a4f291fcb22feb8aaabb64a210d5a53
danielangelo1 <danielangelo1234@gmail.com>
2025-04-06 21:48:53 -0300

    style: format status api code

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -7,27 +7,30 @@ const status = async (request: NextApiRequest, response: NextApiResponse) => {
   const databaseVersion = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersion.rows[0].server_version;
 
-
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
-    const databaseName = process.env.POSTGRES_DB;
+  const databaseName = process.env.POSTGRES_DB;
 
-    const databaseOpenedConnectionsResult = await database.query({text:"SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;", values: [databaseName]})
-    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
-  response.status(200).json({ 
+  response.status(200).json({
     updated_at: updatedAt,
-    dependencies: { 
-      database: { 
+    dependencies: {
+      database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
         opened_connections: databaseOpenedConnectionsValue,
-      }
-    }
+      },
+    },
   });
 };
 
