82c11312714436ab705b4db83527efc5805270ab
danielangelo1 <danielangelo1234@gmail.com>
2026-01-31 20:03:06 -0300

    fix: parse POSTGRES_PORT as an integer in database client configuration

--- infra/database.ts
+++ infra/database.ts
@@ -17,7 +17,7 @@ const query = async (queryObject: {}) => {
 async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
